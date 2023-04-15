d4c1d86e1970cbbd4da4a1cdbe714610cbe35a62
dazzlemon <daanill13470@gmail.com>
2023-04-15 22:37:23 +0300

    simplify

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
-"""
-UDC classes predictor for scientific works in english.
-"""
+"""UDC classes predictor for scientific works in english"""
+import pprint
 from parse_args import parse_args
-from mode import TrainingMode, UDCComparisonMode
 
 if __name__ == '__main__':
     mode = parse_args()
-
-    print(mode.mode_name)
-    print(f'\t{mode.text_filename=}')
-    print(f'\t{mode.model_filename=}')
-    if isinstance(mode, TrainingMode):
-        print(f'\t{mode.new_model_filename=}')
-    elif isinstance(mode, UDCComparisonMode):
-        print(f'\t{mode.udc_code=}')
-    else:
-        print(f'\t{mode.new_model_filename=}')
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(mode)
--- src/mode.py
+++ src/mode.py
@@ -1,30 +1,24 @@
 """Data classes for app modes"""
 from abc import ABC
 from dataclasses import dataclass
-from typing import Optional, Final
+from typing import Optional
 
 @dataclass
 class Mode(ABC):
     """Base class for app modes"""
     model_filename: str
     text_filename: str
-		# idk what's the problem with this class
-    # pylint: disable=invalid-name
-    mode_name: Final[str] = ''
 
 @dataclass
 class PredictionMode(Mode):
     """Prediction mode"""
-    mode_name: Final[str] = 'Prediction'
 
 @dataclass
 class UDCComparisonMode(Mode):
     """Prediction and udc comparison mode"""
-    mode_name: Final[str] = 'Prediction and udc comparison'
     udc: Optional[str] = None
 
 @dataclass
 class TrainingMode(Mode):
     """Training mode"""
-    mode_name: Final[str] = 'Training'
     new_model_filename: Optional[str] = None
