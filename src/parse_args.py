1c4bfeeb960a49b5ce601a39d8c5ad9b57a437e4
dazzlemon <daanill13470@gmail.com>
2023-04-15 22:26:06 +0300

    simplify

--- src/parse_args.py
+++ src/parse_args.py
@@ -3,22 +3,23 @@ import argparse
 from mode import Mode, TrainingMode, PredictionMode, UDCComparisonMode
 
 def parse_args() -> Mode:
-    """CLI args receiver for UDC predictor"""
+    """App mode from CLI args"""
     parser = argparse.ArgumentParser(description='UDC Classifier')
-    parser.add_argument(
-        'model_filename',
-        help='the name of the existing model file'
-    )
+    parser.add_argument('model_filename', help='the name of the model file')
     parser.add_argument('text_filename', help='the name of the input text file')
     parser.add_argument(
         '--training',
         dest='new_model_filename',
-        help='the name of the output file for the updated model',
+        help=
+           'If this parameter is specified, the model will be trained.'
+           "--udc can't be used with this this parameter.",
         required=False
     )
     parser.add_argument(
         '--udc',
-        help='the input UDC code to compare against the predicted classes',
+        help=
+            'the input UDC code to compare against the predicted classes'
+            "--training can't be used with this this parameter.",
         required=False
     )
     args = parser.parse_args()
@@ -26,19 +27,18 @@ def parse_args() -> Mode:
     if args.new_model_filename and args.udc:
         raise ValueError("Cannot specify both --training and --udc options.")
 
+    arguments = {
+        "model_filename": args.model_filename,
+        "text_filename": args.text_filename
+    }
+
     if args.new_model_filename:
-        return TrainingMode(
-            model_filename=args.model_filename,
-            text_filename=args.text_filename,
-            new_model_filename=args.new_model_filename
-        )
-    if args.udc:
-        return UDCComparisonMode(
-            model_filename=args.model_filename,
-            text_filename=args.text_filename,
-            udc=args.udc
-        )
-    return PredictionMode(
-        model_filename=args.model_filename,
-        text_filename=args.text_filename
-    )
+        arguments["new_model_filename"] = args.new_model_filename
+        mode = TrainingMode
+    elif args.udc:
+        arguments["udc"] = args.udc
+        mode = UDCComparisonMode
+    else:
+        mode = PredictionMode
+
+    return mode(**arguments)
