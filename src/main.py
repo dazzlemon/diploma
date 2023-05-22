d015e0f2d49a7e4ef103096dd8352cecb0998dee
dazzlemon <daanill13470@gmail.com>
2023-05-22 18:29:26 +0300

    improv

--- src/main.py
+++ src/main.py
@@ -21,7 +21,17 @@ def extract_keywords(text: UdcPredictorTextInput, top_k=50):
     for chunk in doc.noun_chunks:
         is_valid = all(not token.is_stop for token in chunk if token.is_alpha)
         if is_valid:
-            noun_chunks.append(chunk.text.lower())
+            # Remove punctuation marks from the chunk
+            chunk_text = '' \
+                .join(token.text for token in chunk if not token.is_punct) \
+                .strip() \
+                .lower()
+
+            # I had the same word appear twice, once with a dot at the end
+            chunk_text = chunk_text.rstrip('.')
+
+            if len(chunk_text) > 1:
+                noun_chunks.append(chunk_text)
 
     keyword_counts = Counter(noun_chunks)
     sorted_keywords = keyword_counts.most_common(top_k)
@@ -57,11 +67,11 @@ def main():
     pretty_printer = pprint.PrettyPrinter(indent=4)
     pretty_printer.pprint(mode)
 
-    model = read_model(mode.model_filename)
-    text = read_text(mode.text_filename)
+    print('')
 
-    for w in extract_keywords(text):
-        print(w)
+    text = read_text(mode.text_filename)
+    for word in extract_keywords(text):
+        print(word)
 
 
 if __name__ == '__main__':
