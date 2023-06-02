d439239b27ee485c3519196ebdec9517a66c5238
dazzlemon <daanill13470@gmail.com>
2023-06-02 19:08:13 +0300

    cython wont work lol

--- util/compile_linux.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build/linux
-cp -r src build/linux
-
-gcc \
-  build/main.c \
-  -I/usr/include/python3.10 \
-	-lpython3.10 \
-	-o build/linux/main
--- util/transpile_to_c.sh
+++
@@ -1,6 +0,0 @@
-cython \
-  -3 \
-	--embed \
-	-o build/main.c \
-	-I src \
-	src/main.py
