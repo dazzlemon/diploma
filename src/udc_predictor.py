e7582fb2f1a1353b5a8e772ee635237301173f98
dazzlemon <daanill13470@gmail.com>
2023-06-09 17:25:25 +0300

    pyFunctional

--- src/udc_predictor.py
+++ src/udc_predictor.py
@@ -2,8 +2,8 @@
 Core logic of the application
 """
 
-from collections import Counter
 import spacy
+from functional import seq
 from udc_predictor_text_input import UdcPredictorTextInput
 
 def extract_keywords(text: UdcPredictorTextInput, top_k=50):
@@ -12,26 +12,23 @@ def extract_keywords(text: UdcPredictorTextInput, top_k=50):
     """
     nlp = spacy.load("en_core_web_lg")
     doc = nlp(text)
-    keywords = []
 
-    noun_chunks = []
-    for chunk in doc.noun_chunks:
-        is_valid = all(not token.is_stop for token in chunk if token.is_alpha)
-        if is_valid:
+    return seq(doc.noun_chunks)\
+        .filter(lambda chunk:
+            all(not token.is_stop for token in chunk if token.is_alpha)
+        )\
+        .map(lambda chunk: ' ' \
+            .join(token.text for token in chunk if not token.is_punct) \
+            .strip() \
+            .lower()
             # Remove punctuation marks from the chunk
-            chunk_text = ' ' \
-                .join(token.text for token in chunk if not token.is_punct) \
-                .strip() \
-                .lower()
-
-            # I had the same word appear twice, once with a dot at the end
-            chunk_text = chunk_text.rstrip('.')
-
-            if len(chunk_text) > 1:
-                noun_chunks.append(chunk_text)
-
-    keyword_counts = Counter(noun_chunks)
-    sorted_keywords = keyword_counts.most_common(top_k)
-    keywords = [keyword for keyword, count in sorted_keywords]
-
-    return keywords
+        )\
+        .map(lambda chunk_text: chunk_text.rstrip('.')
+        # I had the same word appear twice, once with a dot at the end
+        )\
+        .filter(lambda chunk_text: len(chunk_text) > 1)\
+        .map(lambda e: (e, 1))\
+        .reduce_by_key(lambda a, b: a + b)\
+        .sorted(lambda x: x[1], True)\
+        .take(top_k)\
+        .map(lambda pair: pair[0])
