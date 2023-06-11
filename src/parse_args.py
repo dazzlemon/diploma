4e830f8c1e92408a7154ca1e52325df29047e88f
dazzlemon <daanill13470@gmail.com>
2023-06-11 22:55:43 +0300

    start of the testing section + small fix in code

--- src/parse_args.py
+++ src/parse_args.py
@@ -13,17 +13,18 @@ def parse_args() -> Mode:
         '--training',
         dest='new_model_filename',
         help='If this parameter is specified, the model will be trained. '
-             'You should also specify --keywords along with it.',
+             'You should also specify --keywords and --udc along with it.',
     )
     parser.add_argument(
         '--udc',
-        help='the input UDC code to compare against the predicted classes',
+        help='the input UDC code to compare against the predicted classes, '
+             'or to be used for trainging (see --training)',
     )
     parser.add_argument(
         '--keywords',
         help='the input keywords to be used for training '
              '(string of words delimeted by a comma). '
-             'You should also specify --training along with it.',
+             'You should also specify --training and --udc along with it.',
     )
     args = parser.parse_args()
 
@@ -35,6 +36,8 @@ def parse_args() -> Mode:
     if args.new_model_filename:
         if args.keywords is None:
             parser.error('--keywords should be specified for training mode')
+        if args.udc is None:
+            parser.error('--udc should be specified for training mode')
         arguments["new_model_filename"] = args.new_model_filename
         arguments["udc"] = UdcCode.from_string(args.udc)
         arguments["keywords"] = Keywords.from_string(args.keywords)
